ccdc1404e045d6535f74e43e9a0de073c177d71d
alsagoob <alsagoob@hotmail.com>
2021-04-17 14:41:37 +0300

    initial comit

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Layout from '../../components/Layout'
+import { getAllPostIds, getPostData} from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData }) {
+
+    return (
+    <Layout>
+        <Head>
+       <title>{postData.title}</title> 
+       </Head>
+       <article>
+           <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        
+        <div className={utilStyles.lightText}>{postData.userId}</div>
+        
+        <div>{postData.body}</div>
+        
+        </article>  
+    </Layout>)
+}
+
+export async function getStaticPaths() {
+    const paths = await getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id)
+
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
